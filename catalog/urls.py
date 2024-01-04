97317e3d4556186c4f549df422bb9ab1708058a5
tatyana lozovaya <tatka8632@gmail.com>
2024-01-04 22:31:19 +0300

    homework_20_2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from catalog.views import index, contacts, home
+
+from catalog.apps import CatalogConfig
+from catalog.views import index, contacts, home, product
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index),
-    path('contacts', contacts),
-    path('home', home),
+    path('', index, name='index'),
+    #path('product/', product, name='product'),
+    path('product/<int:pk>/', product, name='product'),
+    path('contacts/', contacts),
+    #path('home/', home),
+    #path('categories/',categories, name='categories')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from catalog.models import Category, Product
+
 
 def index(request):
-    return render(request, 'catalog/index.html')
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'Магазин продуктов'
+    }
+    return render(request, 'catalog/index.html', context)
+
+def product(request, pk):
+    context = {
+        'object_list': Product.objects.filter(id=pk)
+    }
+    return render(request, 'catalog/product.html', context)
+
+def product_list(request):
+    if request.method == 'POST':
+        # Обработка нажатия кнопки
+        # Вы можете выполнить здесь необходимые действия или перенаправить пользователя на другую страницу
+        return redirect('новая_страница')
+    else:
+        return render(request, 'product_list.html')
+
+# def categories(request):
+#     context = {
+#         'object_list': Category.objects.all(),
+#         'title': 'Магазин продуктов'
+#     }
+#     return render(request, 'catalog/index.html', context)
+
 
 def contacts(request):
     if request.method == 'POST':
