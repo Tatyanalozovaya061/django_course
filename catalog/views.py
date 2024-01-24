7c5ec794774f984186c7381ee19fdf062aecc60f
tatyana lozovaya <tatka8632@gmail.com>
2024-01-24 19:23:32 +0300

    homework_22.1

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,4 @@
+from django.forms import inlineformset_factory
 from django.urls import reverse_lazy
 
 from catalog.forms import ProductForm, VersionForm
@@ -7,16 +8,6 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 
 class ProductListView(ListView):
     model = Product
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     active_versions = Version.objects.filter(is_active=True)
-    #     if active_versions:
-    #         context['active_versions'] = active_versions
-    #     else:
-    #         context['active_versions'] = None
-    #     return context
-
 
 class ProductDetailView(DetailView):
     model = Product
@@ -55,3 +46,26 @@ class VersionCreateView(CreateView):
     model = Version
     form_class = VersionForm
     success_url = reverse_lazy('catalog:list')
+
+
+class VersionUpdateView(UpdateView):
+    model = Version
+    form_class = VersionForm
+    success_url = reverse_lazy('catalog:list')
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
+        if self.request.method == 'POST':
+            context_data['formset'] = VersionFormset(self.request.POST, instance=self.object)
+        else:
+            context_data['formset'] = VersionFormset(instance=self.object)
+        return context_data
+
+    def form_valid(self, form):
+        formset = self.get_context_data()['formset']
+        self.object = form.save()
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+        return super().form_valid(form)
